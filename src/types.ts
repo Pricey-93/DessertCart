ca7426d82db8028767d74d72f7cd087591f49d63
Dan <Daniel-J-Price.dev@pm.me>
2025-06-25 18:42:58 +0100

    refactor: make image types nullable for limiting cart type properties

--- src/types.ts
+++ src/types.ts
@@ -6,8 +6,15 @@ export interface Product {
 }
 
 export interface Image {
-  thumbnail: string,
-  mobile: string,
-  tablet: string,
-  desktop: string
+  thumbnail?: string,
+  mobile?: string,
+  tablet?: string,
+  desktop?: string
+}
+
+export interface CartItem {
+  image: Image,
+  name: string,
+  price: number,
+  quantity: number
 }
