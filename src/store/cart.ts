7993dc90030659633b8d958e298912672edb32e0
Dan <Daniel-J-Price.dev@pm.me>
2025-06-26 19:25:48 +0100

    feat: add functions for button actions

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,37 +1,51 @@
 import { ref } from "vue";
-import type { CartItem } from "../types";
+import type { CartItem, Product } from "../types";
 
-const cartItems = ref<CartItem[]>([
-  {
-    "image": {
-      "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
-    },
-    "name": "Waffle with Berries",
-    "price": 6.5,
-    "quantity": 2
-  },
-  {
-    "image": {
-      "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
-    },
-    "name": "Vanilla Bean Crème Brûlée",
-    "price": 7,
-    "quantity": 5
-  }
-]);
+const cartItems = ref<CartItem[]>([]);
 
 export function useCart() {
-  function add(cartItem: CartItem) {
+  function add(cartItem: CartItem): void {
     cartItems.value.push(cartItem);
   }
 
-  function remove(index: number) {
+  function remove(index: number): void {
     cartItems.value.splice(index, 1);
+  }
+
+  function calculateOrderTotal(): number {
+    return cartItems.value.reduce((orderTotal, item) => orderTotal + item.price * item.quantity, 0);
+  }
+
+  function incrementQuantity(productName: string): void {
+    const match = cartItems.value.find(item => item.name === productName);
+    if (match) {
+      match.quantity += 1;
+    }
+  }
+
+   function decrementQuantity(productName: string): void {
+    const match = cartItems.value.find(item => item.name === productName);
+    if (match) {
+      match.quantity > 1 ? match.quantity -= 1 : 1;
+    }
+  }
+
+  function createCartItem(product: Product): CartItem {
+    return {
+      image: {thumbnail: product.image.thumbnail},
+      name: product.name,
+      price: product.price,
+      quantity: 1
+    } 
   }
 
   return {
     cartItems,
     add,
     remove,
+    calculateOrderTotal,
+    createCartItem,
+    incrementQuantity,
+    decrementQuantity
   };
 }
